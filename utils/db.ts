a0aef8d4de48bb0d77f07baf59b8e80928c131c9
shirayki <wayhq1314@gmail.com>
2024-08-18 14:41:02 +0800

    Update db.ts

--- utils/db.ts
+++ utils/db.ts
@@ -136,18 +136,8 @@ export const textGenModels: Model[] = [{
 }]
 
 export const imageGenModels: Model[] = [{
-    id: '@cf/runwayml/stable-diffusion-v1-5-inpainting',
-    name: 'stable-diffusion-v1-5-inpainting',
-    provider: 'workers-ai-image',
-    type: 'text-to-image'
-}, {
     id: '@cf/stabilityai/stable-diffusion-xl-base-1.0',
     name: 'stable-diffusion-xl-base-1.0',
-    provider: 'workers-ai-image',
-    type: 'text-to-image'
-}, {
-    id: '@cf/runwayml/stable-diffusion-v1-5-img2img',
-    name: 'stable-diffusion-v1-5-img2img',
     provider: 'workers-ai-image',
     type: 'text-to-image'
 }]
